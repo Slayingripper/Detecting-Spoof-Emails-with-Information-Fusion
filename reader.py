7acad1e032db73c4bd2fe2803f992eb81a3da927
psyvi <psyvi@exmail.nottingham.ac.uk>
2019-10-28 17:24:31 +0000

    filtering added with regex

--- reader.py
+++ reader.py
@@ -1,27 +1,30 @@
+#imports 
 import pandas as pd
 import re
 
+
+
+#open CSV FILE and replace empty spaces with ""
 df = pd.read_csv (r'/home/blackfalcon/Detecting-Spoof-Emails-with-Information-Fusion/Dataset/CSVDATA.csv')
 
+#check if SENDER COLUMN is present
 if 'SENDER' in df.columns:
     print("Yes")
 else:
     print("No")
  
- 
-if set(['SENDER','dp']).issubset(df.columns):
+if 'SUBJECT' in df.columns:
     print("Yes")
 else:
     print("No")
 
 print("\n---- Filter DataFrame using & ----\n")
  
-df_filtered = df.query('SUBJECT == "XXX"')
+searchme = df.query('SUBJECT == "XXX"')
  
-print(df_filtered)
+print(searchme)
 
 print("\n --- search string  ---\n")
 
-df[df['SENDER'].str.contains('money')]
+print(df[df.SUBJECT.str.contains('MONEY', regex= True, na=False)])
 
-print(df)
