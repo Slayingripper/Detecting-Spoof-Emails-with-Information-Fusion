97a86f9cdc00824b40f41ba60657e0ae55aa577f
Vasilis <kirav96@gmail.com>
2019-10-28 15:48:53 +0000

    trying to search strings

--- reader.py
+++ reader.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+import re
 
 df = pd.read_csv (r'/home/blackfalcon/Detecting-Spoof-Emails-with-Information-Fusion/Dataset/CSVDATA.csv')
 
@@ -14,3 +14,14 @@ if set(['SENDER','dp']).issubset(df.columns):
 else:
     print("No")
 
+print("\n---- Filter DataFrame using & ----\n")
+ 
+df_filtered = df.query('SUBJECT == "XXX"')
+ 
+print(df_filtered)
+
+print("\n --- search string  ---\n")
+
+df[df['SENDER'].str.contains('money')]
+
+print(df)
