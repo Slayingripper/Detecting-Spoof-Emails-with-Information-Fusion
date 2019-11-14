ca28bfc674693ee1f9d13090c6c5071733e3d60d
psyvi <psyvi@exmail.nottingham.ac.uk>
2019-11-14 15:07:46 +0000

    test

---
+++ tester.py
@@ -0,0 +1,20 @@
+import domaincheck  # The module which contains the call to input
+import domainextcheck
+import grammar
+import reader
+import time
+
+start_time = time.time()
+
+class TestClass:
+
+    def test_function_1(self):
+        domaincheck()
+
+    def test_function_2(self):
+        domainextcheck()      
+
+    def test_function_3(self):
+        grammar()
+start_time = time.time()
+print("--- %s seconds ---" % (time.time() - start_time))
