5d0a4b0da22d368462597986430ac22a1deeae2f
psyvi <psyvi@exmail.nottingham.ac.uk>
2019-11-18 20:07:17 +0000

    english

--- Logistic Regression/spammy.py
+++ Logistic Regression/spammy.py
@@ -9,7 +9,7 @@ from textblob.classifiers import NaiveBayesClassifier
 
 #we calculate the row count and and the training amount we are going to use for 
 #our classifier the current dataset current has around 6k or spam and ham (mixed)
-row_count = len(list(csv.reader(open('/content/drive/My Drive/SMSSpamCollection'))))
+row_count = len(list(csv.reader(open('/home/blackfalcon/Detecting-Spoof-Emails-with-Information-Fusion/Dataset/SMSSpamCollection'))))
 print(row_count)
 #using int to round the train amount (Lower BOUND)
 trainamount = int(row_count/4)
@@ -24,7 +24,7 @@ print(therest)
 #using stop words causes a massive INCREASE in import time so we have to use 
 #a specific one to reduce the time taken , for example "english"
 #if left blank the stopwords function of NLTK searches all of its dictionaries
-#around 14! so if on average it takes 15 seconds to check it will take 210s 
+#around 24! so if on average it takes 11 seconds to check it will take 264s 
 #to check all of them not including sorting them into tuples
 def get_list_tuples(read_file):
 	list_tuples = []
@@ -38,7 +38,7 @@ def get_list_tuples(read_file):
 			except:
 				continue
 			for word in words:
-				if word not in stopwords.words('english') and not word.isdigit():
+				if word not in stopwords.words("english") and not word.isdigit():
 					list_tuples.append((word.upper(),tabsep[0]))
 			c+=1
 			if c== row_count:
@@ -47,7 +47,7 @@ def get_list_tuples(read_file):
 
 print ('importing data...')
 a = time.time()
-entire_data = get_list_tuples("/content/drive/My Drive/SMSSpamCollection")
+entire_data = get_list_tuples("/home/blackfalcon/Detecting-Spoof-Emails-with-Information-Fusion/Dataset/SMSSpamCollection")
 print ("It took "+str(time.time()-a)+" seconds to import data")
 print ('data imported')
 random.seed(1)
