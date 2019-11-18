96dbf2091dfc17c4d1a7fe5d107f8204c10a0f43
psyvi <psyvi@exmail.nottingham.ac.uk>
2019-11-18 19:57:15 +0000

    EFFICIENCY !

--- Logistic Regression/spammy.py
+++ Logistic Regression/spammy.py
@@ -7,14 +7,25 @@ from textblob import TextBlob
 from nltk.corpus import stopwords
 from textblob.classifiers import NaiveBayesClassifier
 
-
-row_count = len(list(csv.reader(open('/home/blackfalcon/Detecting-Spoof-Emails-with-Information-Fusion/Dataset/SMSSpamCollection'))))
+#we calculate the row count and and the training amount we are going to use for 
+#our classifier the current dataset current has around 6k or spam and ham (mixed)
+row_count = len(list(csv.reader(open('/content/drive/My Drive/SMSSpamCollection'))))
 print(row_count)
+#using int to round the train amount (Lower BOUND)
 trainamount = int(row_count/4)
 print(trainamount)
+#Since the train amount is going to be 1/4 of the data set we need to increment
+# by 1 so that we start classifying the next row and until the end of the file
 therest = trainamount + 1
 print(therest)
 
+
+
+#using stop words causes a massive INCREASE in import time so we have to use 
+#a specific one to reduce the time taken , for example "english"
+#if left blank the stopwords function of NLTK searches all of its dictionaries
+#around 14! so if on average it takes 15 seconds to check it will take 210s 
+#to check all of them not including sorting them into tuples
 def get_list_tuples(read_file):
 	list_tuples = []
 	with open(read_file,"r") as r:
@@ -27,24 +38,16 @@ def get_list_tuples(read_file):
 			except:
 				continue
 			for word in words:
-				if word not in stopwords.words() and not word.isdigit():
-					list_tuples.append((word.lower(),tabsep[0]))
+				if word not in stopwords.words('english') and not word.isdigit():
+					list_tuples.append((word.upper(),tabsep[0]))
 			c+=1
-			if c== 500 :   #row_count
+			if c== row_count:
 				break
-	return list_tuples
+		return list_tuples
 
 print ('importing data...')
 a = time.time()
-
-
-
-
-
-entire_data = get_list_tuples("/home/blackfalcon/Detecting-Spoof-Emails-with-Information-Fusion/Dataset/SMSSpamCollection")
-
-
-#entire_data = list(csv.reader(open('/home/blackfalcon/Detecting-Spoof-Emails-with-Information-Fusion/Dataset/SMSSpamCollection')))
+entire_data = get_list_tuples("/content/drive/My Drive/SMSSpamCollection")
 print ("It took "+str(time.time()-a)+" seconds to import data")
 print ('data imported')
 random.seed(1)
