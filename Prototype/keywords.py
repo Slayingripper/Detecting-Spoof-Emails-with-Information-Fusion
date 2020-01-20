5fa74e9d4c43575a8a2610adbe1678d1d1c1bcac
slayingripper <kirav96@gmail.com>
2020-01-20 16:19:27 +0000

    test

--- Prototype/keywords.py
+++ Prototype/keywords.py
@@ -1,28 +1,29 @@
-#list is from https://www.inmotionhosting.com/support/help/support-tickets/spam-prevention-techniques/common-spam-words/
+# list is from https://www.inmotionhosting.com/support/help/support-tickets/spam-prevention-techniques/common-spam-words/
 import pandas as pd
 import csv
 import time
 import re
 
-#with open('/home/blackfalcon/Detecting-Spoof-Emails-with-Information-Fusion/keywords.txt') as keys:
- #   print(keys)
+# with open('/home/blackfalcon/Detecting-Spoof-Emails-with-Information-Fusion/keywords.txt') as keys:
+#   print(keys)
 spamlevel = 0
- 
+
 keyfile = "/home/blackfalcon/Detecting-Spoof-Emails-with-Information-Fusion/2019.txt"
 # testfile = "/home/blackfalcon/Detecting-Spoof-Emails-with-Information-Fusion/Dataset/CSVDATA.csv"
 
-# keys = set(key.lower() for key in 
+# keys = set(key.lower() for key in
 #     re.findall(r'\w+', open(keyfile , "r").readline()))
 
 # for word in keys:
-   
+
 #     matching = [s for s in testfile if any(xs in s for xs in word)]
 #     #print(word)
 #     spamlevel = spamlevel +1
 # print(matching)
 
+
 def read_words(inputfile):
-    with open(inputfile, 'r') as f:
+    with open(inputfile, "r") as f:
         while True:
             buf = f.read(10240)
             if not buf:
@@ -38,10 +39,11 @@ def read_words(inputfile):
             words = buf.split()
             for word in words:
                 yield word
-        yield '' #handle the scene that the file is empty
+        yield ""  # handle the scene that the file is empty
+
 
 if __name__ == "__main__":
     for word in read_words(keyfile):
-        
+
         spamlevel = spamlevel + 1
         print(spamlevel)
